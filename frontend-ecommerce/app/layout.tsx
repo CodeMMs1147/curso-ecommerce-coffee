5a96cc57781311d18a969ba27ab6f3c8f13e5567
juan <code.jfe@gmail.com>
2024-06-26 15:26:46 -0500

    first commit

---
+++ frontend-ecommerce/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CodeMMs Ecommerce",
+  description: "Welcome to my websinte using Next.js 14 Designed for an coffee e-commerce, thanks and have a good day",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
